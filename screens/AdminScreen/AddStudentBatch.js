5e91f7613386b06ad637e36653d2e30d73810a5e
Abu Bakkar Shayan <abubakarshayan511@gmail.com>
2024-04-18 18:19:38 +0500

    file picking

--- screens/AdminScreen/AddStudentBatch.js
+++ screens/AdminScreen/AddStudentBatch.js
@@ -1,15 +1,43 @@
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { Alert, StyleSheet, Text, TextInput, TouchableOpacity, View , Button} from 'react-native'
 import React from 'react'
+import FilePickerManager from 'react-native-file-picker';
+import { request, PERMISSIONS } from 'react-native-permissions';
 
 const AddStudentBatch = () => {
+  const [filePath, setFilePath] = React.useState("");
+  
+  const handlePickFile=async ()=>{
+    const permissioResult=await request(PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE);
+  if (permissioResult !=='granted')
+  {
+    Alert.alert("Permission Required, please grnt me permission to access file");
+    return;
+  }
+    FilePickerManager.showFilePicker(null, async(response)=>{
+      if(response.didCancel){
+        Alert.alert("User Canceled File Picker");
+      }
+      else if(response.error){
+        console.log("Error", response.error)
+;        Alert.alert("An error occured while picking the file");
+      }
+      else{
+        setFilePath(response.path);
+      }
+    })
+  }
+
   return (
     <View style={styles.container}>
         <TextInput
         style={styles.input}
-        placeholder="Name"
+        value={filePath}
+        placeholder="Upload Excel File"
         placeholderTextColor={"#7E7E7E"}
+        editable={false}
       />
-        <TouchableOpacity style={styles.button} ><Text style={styles.buttonText}>Add Batch</Text></TouchableOpacity>
+       <Button title="Upload Excel File" onPress={handlePickFile} />
+        {/* <TouchableOpacity style={styles.button} ><Text style={styles.buttonText}>Add Batch</Text></TouchableOpacity> */}
     </View>
   )
 }
--- screens/TeacherScreen/LoginScreen1.js
+++ screens/TeacherScreen/LoginScreen1.js
@@ -15,7 +15,7 @@ const LoginScreen1 = () => {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch('http://192.168.42.184/FYPAPI/api/user/loginuser', {
+      const response = await fetch('http://192.168.10.5/FYPAPI/api/user/loginuser', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
